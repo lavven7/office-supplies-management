4b8f2ac8a3ae0bb1532114bf810bbf4d53acf7da
lavven7 <lavven777@gmail.com>
2026-06-02 18:00:05 +0900

    init: create spring boot project

---
+++ src/main/java/com/lavven777/officesupplies/OfficeSuppliesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.lavven777.officesupplies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeSuppliesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeSuppliesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavven777/officesupplies/OfficeSuppliesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavven777.officesupplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeSuppliesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
